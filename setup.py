e7380eec2a9bac17d7f41995657bd797304af5bf
Michal Golan <mizarrouk@gmail.com>
2021-06-25 09:59:25 +0300

    install requires

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="dynasty",
     packages=['dynasty'],
+    install_requires=open('requirements.txt').read(),
     description="A Python class hierarchy analyzer",
     long_description=open('README.md').read(),
     author="Michal Golan",

acc9c363f68701c8338505adec9dfc14d813644f
Michal Golan <mizarrouk@gmail.com>
2021-06-25 09:34:32 +0300

    install fixes

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Dynasty extracts the class hierachy for your package, to track down lost or misp
 
 See the [example jupyter notebook](https://github.com/migolan/dynasty/blob/main/example.ipynb) for demonstration.
 
-Michal Golan mizarrouk@gmail.com [@migolan](https://github.com/migolan)
+Michal Golan [migolan@gmail.com](mailto:migolan@gmail.com) [@migolan](https://github.com/migolan)
--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="dynasty",
+    packages=['dynasty'],
+    description="A Python class hierarchy analyzer",
+    long_description=open('README.md').read(),
     author="Michal Golan",
     author_email="migolan@gmail.com",
-    url="https://github.com/migolan/dynasty",
-    packages=['dynasty'],
-    long_description=open('README.md').read()
+    url="https://github.com/migolan/dynasty"
 )

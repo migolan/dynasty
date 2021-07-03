e9ae667cc80647e42b7229f7f8e56325229ee76f
Michal Golan <mizarrouk@gmail.com>
2021-07-03 14:30:56 +0300

    pypi friendly

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     install_requires=open('requirements.txt').read(),
     description="A Python class hierarchy analyzer",
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author="Michal Golan",
     author_email="migolan@gmail.com",
     url="https://github.com/migolan/dynasty"

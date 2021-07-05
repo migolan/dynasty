0f69c779e21a9ead19653c7164b7b465b9b671ce
Michal Golan <mizarrouk@gmail.com>
2021-07-05 19:43:45 +0300

    fixed requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
-import os
-
-this_dir = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_dir, 'requirements.txt'), 'r') as fp:
-    install_reqs = [r.rstrip() for r in fp.readlines()]
 
 setup(
     name="dynasty",
     packages=['dynasty'],
-    install_requires=install_reqs,
+    install_requires=open('requirements.txt').read().splitlines(),
     description="A Python class hierarchy analyzer",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author="Michal Golan",
     author_email="migolan@gmail.com",
-    url="https://github.com/migolan/dynasty"
+    url="https://github.com/migolan/dynasty",
+    version="0.0.2"
 )
